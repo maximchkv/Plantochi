a2f1bef1fed769636e1a0c2572dd0c448237fc3f
mxc <maximchkv@gmail.com>
2026-01-15 00:08:29 +0700

    Initial Commit

---
+++ Plantochi/PlantochiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantochiApp.swift
+//  Plantochi
+//
+//  Created by Max on 15.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct PlantochiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
